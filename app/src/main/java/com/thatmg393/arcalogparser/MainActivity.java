4aee988391ea16d7eda8eec1c7b3372b0773b8b4
ThatMG393 <thatmg393@gmail.com>
2022-08-30 16:03:52 +0800

    Initial commit

---
+++ app/src/main/java/com/thatmg393/arcalogparser/MainActivity.java
@@ -0,0 +1,13 @@
+package com.thatmg393.arcalogparser;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
